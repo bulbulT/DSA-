935bd9d4e62afad5f059423ea0265bb3c5cc8df3
Bulbul Tripathy <67851565+bulbulT@users.noreply.github.com>
2021-08-24 20:17:17 +0530

    Time: 4 ms (80.12%), Space: 9.6 MB (90.21%) - LeetHub

---
+++ search-insert-position/search-insert-position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        //Brute force
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == target){
+                return i;
+            }
+            
+            else if(nums[i] > target){
+                return i;
+            }
+        }
+        
+        return nums.size();
+    }
+};
