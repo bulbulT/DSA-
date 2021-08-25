4139effcded1df11f6d58e96784e2e909cf42edb
Bulbul Tripathy <67851565+bulbulT@users.noreply.github.com>
2021-08-25 13:11:00 +0530

    Update squares-of-a-sorted-array.cpp

--- squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -20,3 +20,28 @@ public:
         return res;
     }
 };
+
+/*
+ #include <bits/stdc++.h>
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res(nums.size());
+        
+        int i = 0;
+        int j = nums.size()-1;
+        
+        for(int k = nums.size() - 1; k >= 0; k--){
+            if(abs(nums[i]) > abs(nums[j])){
+                res[k] = nums[i] * nums[i++];
+            }
+            else{
+                res[k] = nums[j] * nums[j--];
+            }
+        
+        }
+        return res;
+    }
+};
+*/
