2051bf159295ce345b76465e3736ce62a4db0106
Bulbul Tripathy <67851565+bulbulT@users.noreply.github.com>
2021-09-11 11:27:50 +0530

    Update fibonacci-number.cpp

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
+       //Recursive Approach
         if(n == 0 || n == 1){
             return n;
         }
@@ -9,3 +10,15 @@ public:
         
     }
 };
+
+//Memoization
+int fib(int N) {
+        if(N < 2)
+            return N;
+        int memo[N+1];
+        memo[0] = 0;
+        memo[1] = 1;
+        for(int i=2; i<=N; i++)
+            memo[i] = memo[i-1] + memo[i-2];
+        return memo[N];
+    }
