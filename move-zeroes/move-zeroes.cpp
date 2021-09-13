3bc129cc80f03f0f9f45068806d1dc7f01c41ac3
Bulbul Tripathy <67851565+bulbulT@users.noreply.github.com>
2021-09-13 10:12:47 +0530

    Time: 4 ms (88.20%), Space: 9 MB (12.35%) - LeetHub

--- move-zeroes/move-zeroes.cpp
+++ move-zeroes/move-zeroes.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int count = 0;
-        
+        int j = 0;
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] != 0){
-                nums[count++] = nums[i]; 
+                nums[j++] = nums[i];
             }
         }
         
-        for(; count < nums.size(); count++){
-            nums[count] = 0;
+        for(; j < nums.size(); j++){
+            nums[j] = 0;
         }
         
     }
