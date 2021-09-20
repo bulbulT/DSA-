98ea81a2737e927b659420b9440879f5c24e1463
Bulbul Tripathy <67851565+bulbulT@users.noreply.github.com>
2021-09-20 11:35:09 +0530

    Time: 5 ms (7.87%), Space: 6.3 MB (50.77%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,29 @@
+#include <stack>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> visited;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                visited.push(s[i]);
+            }
+            else{
+                if(visited.empty()){
+                    return false;
+                }
+                
+                if(s[i] == ')' && visited.top() != '(')
+                    return false;
+                
+                if(s[i] == ']' && visited.top() != '[')
+                    return false;
+                
+                if(s[i] == '}' && visited.top() != '{')
+                    return false;
+                
+                visited.pop();
+            }
+        }
+        return visited.empty();
+    }
+};
